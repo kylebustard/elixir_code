123aeb7813bf86e1a81d50fb1e2c04f0952b7faa
Kyle Bustard <bustardkyle@gmail.com>
2017-01-27 23:20:29 -0500

    implement double comprehension method to create full deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
-  end
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
+	  for suit <- suits, value <- values do
+	      "#{value} of #{suit}"
+	  end
+  end
+    
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
